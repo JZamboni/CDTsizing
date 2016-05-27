0c672023c3ea87a0277a050044facfa55f1134b0
JZamboni <jacopozamboni@gmail.com>
2016-05-27 15:43:55 +0200

    Primi lavori sull'input

--- CDTsizing/Core/inputHandler.py
+++ CDTsizing/Core/inputHandler.py
@@ -1,3 +1,4 @@
+import os
 from openpyxl import load_workbook
 from Tkinter import *
 from tkFileDialog import askopenfilename
@@ -15,11 +16,25 @@ class inputHandler:
         *xml
     '''
 
-
     def callback():
+        '''
+        GUI to open a desired file.
+        '''
         name = askopenfilename()
-        print name
+        return name
 
     errmsg = 'Error!'
     Button(text='File Open', command=callback).pack(fill=X)
-    mainloop()
+
+    filePath = callback()
+    filename, file_extension = os.path.splitext(filePath)  # File extension for use in the rest of input file.
+
+    if file_extension == ".xlsx":
+        print('file type Excel')
+        var=readExcel()
+    else:
+        print('file type is not supported')
+
+    def readExcel():
+        return 1.0
+
