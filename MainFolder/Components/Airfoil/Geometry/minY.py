8f265d1450e5fbdc920f4b9466b1b708c118cc96
elmancio1 <mancio.am93@gmail.com>
2016-05-31 11:12:09 +0200

    airfoil package

---
+++ MainFolder/Components/Airfoil/Geometry/minY.py
@@ -0,0 +1,14 @@
+from MainFolder.Core.Parameter import parameter
+
+class minY(parameter):
+    '''
+    gives the lowest y value of the airfoil
+
+    :Unit: [ ]
+    '''
+
+
+
+        ###################################################################################################
+        # EOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFE#
+        ###################################################################################################
