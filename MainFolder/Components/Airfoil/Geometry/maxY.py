fe9a34c8c4a1e0673c140810a093acd8c76a5925
elmancio1 <mancio.am93@gmail.com>
2016-05-31 11:12:09 +0200

    airfoil package

---
+++ MainFolder/Components/Airfoil/Geometry/maxY.py
@@ -0,0 +1,14 @@
+from MainFolder.Core.Parameter import parameter
+
+class maxY(parameter):
+    '''
+    gives the highest y value of the airfoil
+
+    :Unit: [ ]
+    '''
+
+
+
+        ###################################################################################################
+        # EOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFEOFE#
+        ###################################################################################################
