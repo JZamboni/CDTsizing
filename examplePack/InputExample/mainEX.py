10cc32851824a7a3429616e479e0dc58c5c67913
JZamboni <jacopozamboni@gmail.com>
2016-06-02 22:34:03 +0200

    ma che oh! non funziona nulla qui!

--- examplePack/InputExample/mainEX.py
+++ examplePack/InputExample/mainEX.py
@@ -1,12 +1,26 @@
 from parapy.core import *
 from MainFolder.Components.Fuselage.FuselageEX import fuselage
+from MainFolder.Components.Fuselage.Geometry.fuselageLength import fuselageLength
 
 class Airplane(Base):
 
-    @Input
-    def variabile(self):
-        return 4.0
 
+    fuselage = fuselageLength()
+
+    @Input
+    def fuselage_Length(self):
+        return 32.
+
+    addizione = fuselage_Length+2
+
+    @Attribute
+    def addit(self):
+        return self.addizione
+    fuselage.setValue(value=fuselage_Length)
+
+    @Attribute
+    def preghiamo(self):
+        return self.fuselage.getValue()
 
 if __name__ == '__main__':
     from parapy.gui import display
